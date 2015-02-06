b0001b58a29aae2d4895256e5968d0366e7e66ce
Student <student@eggimac18.usc.edu>
2015-02-06 12:12:35 -0800

    commit

---
+++ my/my/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  my
+//
+//  Created by Student on 2/2/15.
+//  Copyright student 2015. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ my/my/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  my
+//
+//  Created by Student on 2/2/15.
+//  Copyright student 2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
